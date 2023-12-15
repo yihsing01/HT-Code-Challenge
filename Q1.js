64207f38eabd08354291a2d598b96726b4b37089
yihsing01 <74348874+yihsing01@users.noreply.github.com>
2023-12-15 21:05:36 +0800

    Update Q1.js

--- Q1.js
+++ Q1.js
@@ -1,3 +1,4 @@
+// swap 2 elements in array
 function swap(arr, num1, num2){
     temp = arr[num1];
     arr[num1] = arr[num2];
@@ -5,17 +6,21 @@ function swap(arr, num1, num2){
     return arr;
 }
 
+// find the smallest larger digit to the right
 function findNextMaxId(arr, i) {
     let temp= [];
+    // identify all the elements to the right that are larger than the digit
     for (let j=arr.length-1; j>i-1; j--) {
         if (arr[j] > arr[i-1]) {
             temp = temp.concat(arr[j]);
         }
     }
-    let nextMax = Math.min(...temp);
+    
+    let nextMax = Math.min(...temp);   // identify the smallest digit among those digits
     return arr.indexOf(nextMax);
 }
 
+// reverse the elements of an array in a specific range
 function reverseArrayInRange(arr, i, j) {
     return arr.slice(0, i).concat(arr.slice(i, j + 1).reverse());
 }
@@ -24,12 +29,12 @@ function nextNumber(num) {
     arr = Array.from(String(num), Number);
     arrLen = String(num).length;
 
-    //check if the num is only 1 digit 
+    // return -1 if the num is only 1 digit 
     if(arrLen <= 1){
         return -1;
     }
 
-    //check if all digits are the same
+    // return -1 all digits are the same
     let same = true;
     for(let i=0; i<arrLen; i++){
         if(arr[0] != arr[i]){  
@@ -42,24 +47,25 @@ function nextNumber(num) {
     }
 
     for(i=arrLen-1; i>=0; i--){
-        //triggered if the numbers can't be rearranged to form a bigger number
+        // return -1 if the digits can't be rearranged to form a bigger number
         if(i==0){
             return -1;
         }
-
+        
+        //  identify the first instance where a digit is smaller than the digit on its right
         if(arr[i] > arr[i-1]){
-            nextMaxId = findNextMaxId(arr, i);
-            arr = swap(arr, i-1, nextMaxId);
-            arr = reverseArrayInRange(arr, i, arrLen-1);
-            return parseInt(arr.join(''));
+            nextMaxId = findNextMaxId(arr, i);              // find the smallest larger digit to the right
+            arr = swap(arr, i-1, nextMaxId);                // swap the two digits
+            arr = reverseArrayInRange(arr, i, arrLen-1);    // sort the remaining digits ascendingly
+            return parseInt(arr.join(''));                  // convert to int and return answer
         }
         
     }
 }
 
-console.log(12, nextNumber(12));   // returns 21
-console.log(513, nextNumber(513));   // returns 531
-console.log(2017, nextNumber(2017));   // returns 2071
-console.log(9, nextNumber(9));       // returns -1
-console.log(111, nextNumber(111));     // returns -1
-console.log(531, nextNumber(531));         // returns -1
+console.log(12, nextNumber(12));        // returns 21
+console.log(513, nextNumber(513));      // returns 531
+console.log(2017, nextNumber(2017));    // returns 2071
+console.log(9, nextNumber(9));          // returns -1
+console.log(111, nextNumber(111));      // returns -1
+console.log(531, nextNumber(531));      // returns -1
