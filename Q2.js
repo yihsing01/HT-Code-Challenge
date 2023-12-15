32690cc8efd4c61fdf70f5729dafd1a4e13c2d97
yihsing01 <74348874+yihsing01@users.noreply.github.com>
2023-12-15 21:05:55 +0800

    Update Q2.js

--- Q2.js
+++ Q2.js
@@ -1,3 +1,4 @@
+// display array correctly
 function display2darr(arr) {
     console.log("arr:")
     for (let i = 0; i < arr.length; i++) {
@@ -11,26 +12,26 @@ function arrange(arr){
     let row = arr.length;
     let col = arr[0].length;
     let ans = [];
-    let iIdx = 0;
-    let jIdx = 0;
+    let iIdx = 0;       // i index for current element
+    let jIdx = 0;       // j index for current element
     let count = 0;
     let cont = true;
-    let dir = 'R';
+    let dir = 'R';      // initialise current direction
 
     while(cont == true){
         if(dir == 'R'){
-            for(let i=0; i<row; i++){
-                if(arr[iIdx][jIdx] != '-'){
-                    ans = ans.concat(arr[iIdx][jIdx]);
-                    arr[iIdx][jIdx] = '-';
-                    count++;
-                    if(jIdx < row-1 && arr[iIdx][jIdx+1]!='-'){
-                        jIdx++;
+            for(let i=0; i<row; i++){                               // loop through all elements in the current direction
+                if(arr[iIdx][jIdx] != '-'){                         // check if current element is recorded
+                    ans = ans.concat(arr[iIdx][jIdx]);              // append current element to answer array
+                    arr[iIdx][jIdx] = '-';                          // set the position of current element as boundary
+                    count++;                                        // increment counter by 1 unit
+                    if(jIdx < row-1 && arr[iIdx][jIdx+1]!='-'){     // check if the next element in the current direction is a boundary or has been recorded
+                        jIdx++;                                     // if not, traverse in the same direction by one unit
                     }
                 }
             }
-            iIdx++;
-            dir = 'D';
+            iIdx++;                                                 // traverse to the element in the next direction
+            dir = 'D';                                              // change to the next direction
         }
         else if(dir=='D'){
             for(let i=0; i<col; i++){
@@ -75,9 +76,9 @@ function arrange(arr){
             dir = 'R';
         }
 
-        if(count == row * col){
-            cont = false;
-            console.log("\nans:\n[" + ans + "]\n");
+        if(count == row * col){                     // check if all elements have been recordeed
+            cont = false;                           // end the loop
+            console.log("\nans:\n[" + ans + "]\n"); // display the answer
             return;
         }
     }
